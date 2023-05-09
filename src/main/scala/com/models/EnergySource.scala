916d0265e84128ee01abd6c55a24dee6a3db4934
kongbai23330 <109857471+kongbai23330@users.noreply.github.com>
2023-05-09 22:22:35 +0300

    Add files via upload

---
+++ src/main/scala/com/models/EnergySource.scala
@@ -0,0 +1,13 @@
+package com.models
+
+trait EnergySource {
+  def id: String
+  def capacity: Double
+  def status: String
+}
+
+case class SolarPanel(id: String, capacity: Double, status: String) extends EnergySource
+
+case class WindTurbine(id: String, capacity: Double, status: String) extends EnergySource
+
+case class HydroPower(id: String, capacity: Double, status: String) extends EnergySource
