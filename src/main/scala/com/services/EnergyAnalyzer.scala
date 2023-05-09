fb2ab82a46a314c3552325c55ce9d9a174d11170
kongbai23330 <109857471+kongbai23330@users.noreply.github.com>
2023-05-09 22:23:20 +0300

    Add files via upload

--- src/main/scala/com/services/EnergyAnalyzer.scala
+++ src/main/scala/com/services/EnergyAnalyzer.scala
@@ -20,4 +20,18 @@ class EnergyAnalyzer {
       .map { case (month, data) => MonthProduction(month, data.map(_.energy).sum) }
       .toSeq
   }
+
+  def mean(data: Seq[Double]): Double = data.sum / data.length
+
+  def median(data: Seq[Double]): Double = {
+    val sorted = data.sorted
+    if (data.length % 2 == 0) (sorted(data.length / 2 - 1) + sorted(data.length / 2)) / 2.0
+    else sorted(data.length / 2)
+  }
+
+  def mode(data: Seq[Double]): Double = data.groupBy(identity).maxBy(_._2.size)._1
+
+  def range(data: Seq[Double]): Double = data.max - data.min
+
+  def midrange(data: Seq[Double]): Double = (data.max + data.min) / 2.0
 }
