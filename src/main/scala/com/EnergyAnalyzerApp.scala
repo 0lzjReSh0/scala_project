6ffded70e195960b04bc870f8d9c4a7110d31d79
kongbai23330 <109857471+kongbai23330@users.noreply.github.com>
2023-05-09 22:25:11 +0300

    Add files via upload

--- src/main/scala/com/EnergyAnalyzerApp.scala
+++ src/main/scala/com/EnergyAnalyzerApp.scala
@@ -1,10 +1,23 @@
 package com
 
-import com.models.{HourProduction, WeekProduction}
-import com.services.EnergyAnalyzer
+import com.models.{HourProduction, HydroPower, SolarPanel, WeekProduction, WindTurbine}
+import com.services.{AlertSystem, EnergyAnalyzer}
+import com.utils.DataStorage
+import com.view.DataView
 
 object EnergyAnalyzerApp extends App {
   val energyAnalyzer = new EnergyAnalyzer
+  val alertSystem = new AlertSystem
+
+  // Initialize some energy sources
+  val sources = Seq(
+    SolarPanel("SP1", 100.0, "operational"),
+    WindTurbine("WT1", 200.0, "operational"),
+    HydroPower("HP1", 300.0, "operational")
+  )
+
+  // Check the status of energy sources
+  alertSystem.checkStatus(sources)
 
   val hourlyData = Seq(
     HourProduction(hour = 1629500400000L, energy = 150),
@@ -15,8 +28,13 @@ object EnergyAnalyzerApp extends App {
     HourProduction(hour = 1629504000000L, energy = 250),
   )
 
+  // Save data
+  DataStorage.saveData(hourlyData)
 
-  val dailyData = energyAnalyzer.analyzeHourlyData(hourlyData)
+  // Read data
+  val readData = DataStorage.readData()
+
+  val dailyData = energyAnalyzer.analyzeHourlyData(readData)
   println(dailyData)
 
   val weeklyData = energyAnalyzer.analyzeDailyData(dailyData)
@@ -24,4 +42,14 @@ object EnergyAnalyzerApp extends App {
 
   val monthlyData = energyAnalyzer.analyzeWeeklyData(weeklyData)
   println(monthlyData)
+
+  // Analyze data
+  println(s"Mean: ${energyAnalyzer.mean(hourlyData.map(_.energy))}")
+  println(s"Median: ${energyAnalyzer.median(hourlyData.map(_.energy))}")
+  println(s"Mode: ${energyAnalyzer.mode(hourlyData.map(_.energy))}")
+  println(s"Range: ${energyAnalyzer.range(hourlyData.map(_.energy))}")
+  println(s"Midrange: ${energyAnalyzer.midrange(hourlyData.map(_.energy))}")
+
+  // Show data
+  DataView.showData(monthlyData)
 }
